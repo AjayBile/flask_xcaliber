8431814ac3b26d7d074ed4214d575588ae5733ca
AjayBile <bile.ajay@yahoo.com>
2020-07-12 18:40:03 +0530

    changed ip and port part

--- run.py
+++ run.py
@@ -1,4 +1,6 @@
 from NewsApp import app
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.getenv('PORT', 5000))
+    print("Starting app on port %d" % port)
+    app.run(debug=False, port=port, host='0.0.0.0')
